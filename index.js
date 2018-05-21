8c1a5127d8d2fb773828c7a7bcb7f31827e221a1
Pamela <PamelaYuhas@gmail.com>
2018-05-21 10:55:26 -0600

    fixed indentation and returns single

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const data        = require('./data.json')
 const app         = express()
 const PORT        = process.env.PORT || 3000
 
-
+app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors({origin: true, credentials: true}))
 
 function filterById(data, id){
-  return data.filter(instructor => instructor.ID == id)
+  return data.filter(instructor => instructor.id == id)
 }
 
-app.get('/', (req, res) => res.json({data: data}))
+app.get('/', (req, res) => res.json({data}))
 
 app.get('/:id', (req, res) => {
   let result = filterById(data, req.params.id)
   if (!result[0]){
-        res.status(404).json({
-            error: {
-                message: "No record found!"
-            }
-        })
-    } else {
-        res.json({data: result})
+    res.status(404).json({
+      error: {
+        message: "No record found!"
       }
+    })
+  } else {
+    res.json({
+    data: result
+    })
+  }
 })
 
-app.listen(PORT, () => console.log('Listening!'))
+app.listen(PORT)
